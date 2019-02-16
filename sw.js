14f9c5fd715960b225fe60ec2ec81060e63baacf
vijayalakshmi <karri.vijayalakshmi2101@gmail.com>
2019-02-16 14:57:27 +0530

    'hi'

---
+++ sw.js
@@ -0,0 +1,28 @@
+var cache1="c2";
+this.addEventListener("install",function(event){
+	event.waitUntil(
+		caches.open(cache1).then(cache=>{
+			cache.addAll(["style.css","form.html","resume.html"
+				])
+		})
+
+
+		);
+});
+
+this.addEventListener('fetch',function(event){
+	event.respondWith(
+		caches.open(cache1).then(function(cache){
+			return cache.match(event.request).then(function(res){
+				return res || fetch(event.request).then(function(reqres){
+					cache.put(event.request,reqres.clone());
+				})
+			})
+
+		})
+
+
+
+		)
+
+})
